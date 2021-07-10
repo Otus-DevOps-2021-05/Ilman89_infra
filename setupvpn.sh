bdef795e6fc3a3b10d23f8805dad70e9828766bd
Ilman89 <man.ilyas@gmail.com>
2021-07-10 03:49:46 +0300

    Cloud-bastion

---
+++ setupvpn.sh
@@ -0,0 +1,11 @@
+cat <<EOF> setupvpn.sh
+#!/bin/bash
+echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/3.6 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.6.list
+echo "deb http://repo.pritunl.com/stable/apt bionic main" | sudo tee /etc/apt/sources.list.d/pritunl.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt update
+sudo apt --assume-yes install pritunl mongodb-server
+sudo systemctl start pritunl mongodb
+sudo systemctl enable pritunl mongodb
+EOF
